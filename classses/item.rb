9986b6b224f5be7703ae3302f006b2295b956a81
Feliperto <fperezferraro@gmail.com>
2023-10-25 11:44:29 -0300

    Fixed linter errors

--- classses/item.rb
+++ classses/item.rb
@@ -9,17 +9,17 @@ class Item
     @archived = false
   end
 
-  def add_author(author) 
+  def add_author(author)
     @author = author
     author.items << self unless author.items.include?(self)
   end
 
-  def add_genre(genre) 
+  def add_genre(genre)
     @genre = genre
     genre.items << self unless genre.items.include?(self)
   end
 
-  def add_label(label) 
+  def add_label(label)
     @label = label
     label.items << self unless label.items.include?(self)
   end

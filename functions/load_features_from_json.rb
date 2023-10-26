5e9a6a171f56db1f1acbf93a21f72dc1be36ac9d
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 09:23:42 +0430

    load author method added

--- functions/load_features_from_json.rb
+++ functions/load_features_from_json.rb
@@ -1,7 +1,29 @@
 require 'json'
+require './classes/author'
 require './classes/label'
+require './classes/genre'
 
 module LoadFeaturesFromJson
+  def load_authors_from_json
+    return unless File.exist?('./authors.json')
+
+    authors_file = File.read('./authors.json')
+    authors_data = JSON.parse(authors_file)
+    authors_data.each do |author|
+      @authors << Author.new(author['first_name'], author['last_name'], author['id'])
+    end
+  end
+
+  def load_genres_from_json
+    return unless File.exist?('./genres.json')
+
+    genres_file = File.read('./genres.json')
+    genres_data = JSON.parse(genres_file)
+    genres_data.each do |genre|
+      @genres << Genre.new(genre['name'], genre['id'])
+    end
+  end
+  
   def load_labels_from_json
     return unless File.exist?('./labels.json')
 

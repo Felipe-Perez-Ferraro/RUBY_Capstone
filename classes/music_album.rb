c6deb7d73ae0605c1fec911f954cea55c931bf0c
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 08:03:03 +0430

    linter solved

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,17 +1,14 @@
-require_relative 'item'
-​
 class MusicAlbum < Item
   attr_accessor :publish_date, :on_spotify, :genre_id, :genre_name, :id, :author, :label
 
-  ​
-  def initialize(publish_date, on_spotify, genre_id, genre_name, _id = rand(1..900))
+  def initialize(publish_date, on_spotify, genre_id, genre_name, id = rand(1..900))
     super(publish_date)
     @id = id
     @on_spotify = on_spotify
     @genre_id = genre_id
     @genre_name = genre_name
   end
-  ​
+
   def can_be_archived?
     super && @on_spotify
   end

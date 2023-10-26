f2346b67c951bc0c0fc3ae32f9b97d9636b7b137
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 08:41:25 +0430

    list music album mehtod added

--- functions/list_items.rb
+++ functions/list_items.rb
@@ -4,14 +4,12 @@ module ListItems
       puts 'There is any book yet...'
     else
       @books.each_with_index do |book, index|
-
         print "\n#{index}) BookId: #{book.id} | Publish Date: #{book.publish_date} | Cover State: #{book.cover_state} |"
         print " Label: #{book.label.title} | Publisher: #{book.publisher}\n"
-
       end
     end
   end
-
+​
   def list_games
     if @games.empty?
       puts 'There is any game yet...'
@@ -20,7 +18,17 @@ module ListItems
         print "\n#{index}) GameId: #{game.id} | Publish Date: #{game.publish_date} | "
         print "Multiplayer: #{game.multiplayer} | Last Played: #{game.last_played_at} | "
         print "Label: #{game.label.title}\n"
-
+      end
+    end
+  end
+​
+  def list_music_albums
+    if @music_albums.empty?
+      puts 'There is no music albums...'
+    else
+      @music_albums.each_with_index do |music_album, index|
+        print "\n#{index}) Id: #{music_album.id} | Publish Date: #{music_album.publish_date}"
+        print " | On Spotify: #{music_album.on_spotify}"
       end
     end
   end

b8bf09700a9da5dba4c7dd5e707533d3612329b0
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 08:12:31 +0430

    linter solved\

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,14 +1,15 @@
 class Genre
-    attr_accessor :name, :items, :id
+  attr_accessor :name, :items, :id
+
   ​
-    def initialize(name, id = rand(1..900))
-      @id = id
-      @name = name
-      @items = []
-    end
-  ​
-    def add_item(item)
-      @items << item
-      item.genre = self
-    end
+  def initialize(name, id = rand(1..900))
+    @id = id
+    @name = name
+    @items = []
   end
+  ​
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
+end

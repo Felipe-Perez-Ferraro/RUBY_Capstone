e1389de4811f1f027c4026832ae062f0570fe82b
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 08:09:35 +0430

    genre class created

---
+++ classes/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+    attr_accessor :name, :items, :id
+  ​
+    def initialize(name, id = rand(1..900))
+      @id = id
+      @name = name
+      @items = []
+    end
+  ​
+    def add_item(item)
+      @items << item
+      item.genre = self
+    end
+  end

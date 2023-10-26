96987960803ead3752d0d18c3bf43871ff0ca679
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 08:40:27 +0430

    methods added in list_features file

--- functions/list_features.rb
+++ functions/list_features.rb
@@ -1,4 +1,24 @@
 module ListFeatures
+  def list_authors
+    if @authors.empty?
+      puts 'No authors found!'
+    else
+      @authors.each_with_index do |author, index|
+        puts "#{index}) Id: #{author.id} | First Name: #{author.first_name} | Last Name: #{author.last_name}"
+      end
+    end
+  end
+​
+  def list_genres
+    if @genres.empty?
+      puts 'No genres found!'
+    else
+      @genres.each_with_index do |genre, index|
+        puts "#{index}) Id: #{genre.id} | Genre: #{genre.name}"
+      end
+    end
+  end
+​
   def list_labels
     if @labels.empty?
       puts 'There is no labels yet...'

1d875877334354bb88a445192fa1ba2f0f340095
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 09:30:09 +0430

    save_music_albums_to_json mehtod and other mehtods added

--- functions/save_items_to_json.rb
+++ functions/save_items_to_json.rb
@@ -10,7 +10,10 @@ module SaveItemsToJson
           'publish_date' => book.publish_date,
           'publisher' => book.publisher,
           'cover_state' => book.cover_state,
-          'label_id' => book.label.id }
+          'author_id' => book.author.id, 
+          'genre_id' => book.genre.id, 
+          'label_id' => book.label.id, 
+        }
       }.to_json
     end
   end
@@ -23,7 +26,24 @@ module SaveItemsToJson
           'publish_date' => game.publish_date,
           'multiplayer' => game.multiplayer,
           'last_played_at' => game.last_played_at,
-          'label_id' => game.label.id
+          'author_id' => game.author.id,
+          'genre_id' => game.genre.id,
+          'label_id' => game.label.id,
+        }
+      }.to_json
+    end
+  end
+
+  def save_music_albums_to_json
+    File.open('./music_albums.json', 'w') do |file|
+      file.puts @music_albums.map { |music_album| 
+        {
+          'id' => music_album.id,
+          'publish_date' => music_album.publish_date,
+          'on_spotify' => music_album.on_spotify,
+          'author_id' => music_album.author.id,
+          'genre_id' => music_album.genre.id,
+          'label_id' => music_album.label.id,
         }
       }.to_json
     end

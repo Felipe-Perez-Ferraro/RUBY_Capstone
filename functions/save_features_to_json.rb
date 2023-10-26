ca04829aebae51e6a555e5ef4edf2139b86d2916
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-26 09:26:30 +0430

    save author to json and some other method added

--- functions/save_features_to_json.rb
+++ functions/save_features_to_json.rb
@@ -2,6 +2,29 @@ require 'json'
 require './classes/label'
 
 module SaveFeaturesToJson
+  def save_authors_to_json
+    File.open('./authors.json', 'w') do |file|
+      file.puts @authors.map { |author| 
+        {
+          'id' => author.id,
+          'first_name' => author.first_name,
+          'last_name' => author.last_name,
+        }
+      }.to_json
+    end 
+  end
+
+  def save_genres_to_json
+    File.open('./genres.json', 'w') do |file|
+      file.puts @genres.map { |genre| 
+        {
+          'id' => genre.id,
+          'name' => genre.name,
+        }
+      }.to_json
+    end
+  end
+
   def save_labels_to_json
     File.open('./labels.json', 'w') do |file|
       file.puts @labels.map { |label|
@@ -13,4 +36,5 @@ module SaveFeaturesToJson
       }.to_json
     end
   end
+
 end
